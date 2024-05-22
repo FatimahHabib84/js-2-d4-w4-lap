608d598c2ef8c4ffd0caa67a960f62ac42d72e2d
FatimahHabib84 <fatimahhabib84@gmail.com>
2024-05-22 17:32:26 +0300

    lap1

--- lap1.js
+++ lap1.js
@@ -12,3 +12,20 @@ for(i=0 ; i<=10 ; i++){
 for( i=1 ; i<=20 ; i+=2 ){
     console.log(i)
 }
+
+// Fibonacci sequence:
+fibonacciSequence = 0
+temp=1
+for(i = 0 ; i<=10 ; i++){
+    temp = fibonacciSequence
+    fibonacciSequence += i
+    fibonacciSequence=temp
+    console.log(`${fibonacciSequence} , `)
+}
+
+// Multiplication table
+for(var i=0 ; i<=3 ; i++){
+    for(var j=0; j<=3 ; j++){
+        console.log(i*j)
+    }
+}
